f70c89896e60e37b5313f13ec56a4a760942d1d2
Stacy Bates (stacbats) <44994840+stacbats@users.noreply.github.com>
2024-08-23 17:47:09 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame, random
+
+#Initialize pygame
+pygame.init()
+
+#Set display surface
+WINDOW_WIDTH = 945
+WINDOW_HEIGHT = 600
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Deflector")
